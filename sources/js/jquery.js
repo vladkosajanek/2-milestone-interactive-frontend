dafa7c1c63239faaafd585aabbb019c43fa2330e
Ubuntu <ubuntu@ip-172-31-42-117.ec2.internal>
2019-06-12 07:41:35 +0000

    jquery accordion installng

---
+++ sources/js/jquery.js
@@ -0,0 +1,8 @@
+//jquery-ui-accordion
+jQuery(document).ready(function($) {
+    $("#accordion").accordion({
+        collapsible: true,
+        active: false,
+        heightStyle: "content"
+    });
+});
